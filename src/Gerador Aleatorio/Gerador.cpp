216b64e05b383e24dd12ee2bc55ec028069e63ea
Iasmim Cunha <iasmim.souza26@gmail.com>
2013-04-29 00:04:51 -0300

    Adicionado transformada de Hartley e modificada fourier

---
+++ src/Gerador Aleatorio/Gerador.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : Gerador.cpp
+// Author      : Iasmim
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdlib.h> // para gerar numeros aleatorios
+#include <time.h> // para gerar a 'random seed' baseada no tempo
+#include <fstream>
+using namespace std;
+
+int main() {
+	int i;
+	cout<<"valor de N"<<endl;
+	cin>>i;
+
+	srand (time (NULL)); // Gera uma 'random seed' baseada no retorno da funcao time()
+	int numero;
+	 // Retorna um numero aleatorio entre 0 e 10
+
+	ofstream fout("aleatorios.txt"); // Cria arquivo para gravação
+	 //em modo texto
+	for (int j = 0; j < i; j++) {
+		numero = rand () % i;
+		fout << numero<<"\n" ;
+	}
+
+}
